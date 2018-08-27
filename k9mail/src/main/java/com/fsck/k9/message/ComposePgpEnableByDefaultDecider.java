2be63fee9a901a96908ef032b6135883156cc67f
koh <koh@cs.columbia.edu>
2018-08-27 12:51:51 -0400

    Do not PGP encrypt outbound messages responding to E3-encrypted emails

--- k9mail/src/main/java/com/fsck/k9/message/ComposePgpEnableByDefaultDecider.java
+++ k9mail/src/main/java/com/fsck/k9/message/ComposePgpEnableByDefaultDecider.java
@@ -3,18 +3,27 @@ package com.fsck.k9.message;
 
 import java.util.List;
 
+import com.fsck.k9.crypto.E3Constants;
 import com.fsck.k9.crypto.MessageCryptoStructureDetector;
+import com.fsck.k9.mail.Flag;
 import com.fsck.k9.mail.Message;
 import com.fsck.k9.mail.Part;
+import com.fsck.k9.mail.internet.MimeMessage;
 
 
 public class ComposePgpEnableByDefaultDecider {
     public boolean shouldEncryptByDefault(Message localMessage) {
-        return messageIsEncrypted(localMessage);
+        return messageIsEncrypted(localMessage) && !messageIsE3Encrypted(localMessage);
     }
 
     private boolean messageIsEncrypted(Message localMessage) {
         List<Part> encryptedParts = MessageCryptoStructureDetector.findMultipartEncryptedParts(localMessage);
         return !encryptedParts.isEmpty();
+    }
+
+    private boolean messageIsE3Encrypted(Message localMessage) {
+        return localMessage.isSet(Flag.E3)
+                || (localMessage instanceof MimeMessage
+                && localMessage.getHeader(E3Constants.MIME_E3_ENCRYPTED_HEADER).length > 0);
     }
 }
