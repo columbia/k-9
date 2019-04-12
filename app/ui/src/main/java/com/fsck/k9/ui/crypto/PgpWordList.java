2b4ea3647f7d58b7180df14ae14278db5c9e80af
unknown <koh@cs.columbia.edu>
2019-04-12 11:22:06 -0400

    Update more E3 strings, and improve E3 PGP Word List implementation and add odd list

--- app/ui/src/main/java/com/fsck/k9/ui/crypto/PgpWordList.java
+++ app/ui/src/main/java/com/fsck/k9/ui/crypto/PgpWordList.java
@@ -8,15 +8,24 @@ import com.fsck.k9.ui.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class PgpWordList {
 
     private static List<String> EVEN_WORDS = new ArrayList<>();
+    private static List<String> ODD_WORDS = new ArrayList<>();
+    private final Random random;
 
     public PgpWordList(Context context) {
         EVEN_WORDS = Arrays.asList(context.getResources().getString(R.string.pgp_word_list_even).split(" "));
+        ODD_WORDS = Arrays.asList(context.getResources().getString(R.string.pgp_word_list_odd).split(" "));
+
+        if (EVEN_WORDS.size() != ODD_WORDS.size()) {
+            throw new IllegalStateException("Got invalid PGP word list sizes");
+        }
+
+        random = new Random();
     }
 
     public List<String> getRandomWords(final int size) {
@@ -24,8 +33,15 @@ public class PgpWordList {
             throw new IllegalArgumentException("invalid size value: " + size);
         }
 
-        Collections.shuffle(EVEN_WORDS);
+        final List<String> selectedWords = new ArrayList<>();
 
-        return EVEN_WORDS.subList(0, size);
+        int i = 0;
+        while (i < size) {
+            int j = random.nextInt(EVEN_WORDS.size());
+            selectedWords.add(i % 2 == 0 ? EVEN_WORDS.get(j) : ODD_WORDS.get(j));
+            ++i;
+        }
+
+        return selectedWords;
     }
 }
